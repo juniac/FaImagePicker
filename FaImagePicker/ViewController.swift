ffcecf2cf91193e62c0b039af93866948495895a
Jeong-un, Kim <juniac79@gmail.com>
2015-07-16 15:17:11 +0900

    Initial Commit

---
+++ FaImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaImagePicker
+//
+//  Created by juniac on 07/16/2015.
+//  Copyright (c) 2015 maneuling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
